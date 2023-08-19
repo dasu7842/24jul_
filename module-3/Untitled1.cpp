ca1b4ad26135957a9571aff84782131fee30f6f5
dasu7842 <140416425+dasu7842@users.noreply.github.com>
2023-08-19 17:18:22 +0530

    19-08-2023

---
+++ module-3/Untitled1.cpp
@@ -0,0 +1,12 @@
+// assigment question
+#include <stdio.h>
+
+int main() {
+    
+    printf("name-darshan zapadiya");
+    printf("\nbirth date-06-03-2003");
+    printf("\nage-20");
+    printf("\naddress-botad");
+
+    return 0;
+}
