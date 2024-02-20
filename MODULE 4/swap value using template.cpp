f70c8b448a2bf060a34e107c65f0a63417b96732
dasu7842 <140416425+dasu7842@users.noreply.github.com>
2024-02-20 18:40:26 +0530

    Update swap value using template.cpp

--- MODULE 4/swap value using template.cpp
+++ MODULE 4/swap value using template.cpp
@@ -1,11 +1,12 @@
 // Write a program of to swap the two values using template
 #include <iostream>
-using namespace std;
-template <typename T>
+using namespace std;  //library file
+template <typename T>  //class
 
 class Swapper
 {
 public:
+ // Constructor to swap two values
     
     Swapper(T &x, T &y)
     {
