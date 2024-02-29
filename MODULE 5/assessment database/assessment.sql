9b6da39e846f1d87ba9c46f0cf861e57d59a4af1
dasu7842 <140416425+dasu7842@users.noreply.github.com>
2024-02-29 18:03:29 +0530

    Add files via upload

---
+++ MODULE 5/assessment database/assessment.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 29, 2024 at 01:29 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `assessment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accountholder`
+--
+
+CREATE TABLE `accountholder` (
+  `account_holder_id` int(11) NOT NULL,
+  `account_no` int(11) DEFAULT NULL,
+  `account_holder_name` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `contact` varchar(20) DEFAULT NULL,
+  `date_created` date DEFAULT NULL,
+  `account_status` varchar(10) DEFAULT NULL,
+  `account_type` varchar(20) DEFAULT NULL,
+  `balance` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank`
+--
+
+CREATE TABLE `bank` (
+  `branch_id` int(11) NOT NULL,
+  `branch_name` varchar(50) DEFAULT NULL,
+  `branch_city` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loan`
+--
+
+CREATE TABLE `loan` (
+  `loan_no` int(11) NOT NULL,
+  `branch_id` int(11) DEFAULT NULL,
+  `account_holder_id` int(11) DEFAULT NULL,
+  `loan_amount` decimal(10,2) DEFAULT NULL,
+  `loan_type` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accountholder`
+--
+ALTER TABLE `accountholder`
+  ADD PRIMARY KEY (`account_holder_id`);
+
+--
+-- Indexes for table `bank`
+--
+ALTER TABLE `bank`
+  ADD PRIMARY KEY (`branch_id`);
+
+--
+-- Indexes for table `loan`
+--
+ALTER TABLE `loan`
+  ADD PRIMARY KEY (`loan_no`),
+  ADD KEY `branch_id` (`branch_id`),
+  ADD KEY `account_holder_id` (`account_holder_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `loan`
+--
+ALTER TABLE `loan`
+  ADD CONSTRAINT `loan_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `bank` (`branch_id`),
+  ADD CONSTRAINT `loan_ibfk_2` FOREIGN KEY (`account_holder_id`) REFERENCES `accountholder` (`account_holder_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
